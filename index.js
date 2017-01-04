dda9fdafa6c73bbfcbfaea04d10a11b763cf675e
BrianLee <briankigondu@gmail.com>
2017-01-04 22:34:03 +0300

    Added Button Functionality for time
    can increase and decrease the length of sessions and time

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-<!DOCTYPE html>
-<html>
-	<head></head>
-</html>
+// mustache the numbers, add on click event to + & -, write functions to handle the data
+
+var lengthTimeAdjuster = new Vue({
+	el:'#length-time-adjuster',
+	data:{
+		breaklength:5,
+		sessionlength:25
+	},
+	methods:{
+		minusBreakLength: function(){
+			this.breaklength = this.breaklength - 1 
+		},
+		addBreakLength: function(){
+			this.breaklength = this.breaklength + 1
+		},
+		minusSessionLength: function(){
+			this.sessionlength = this.sessionlength - 1 
+		},
+		addSessionLength: function(){
+			this.sessionlength = this.sessionlength + 1 
+		},
+	}
+})
+
